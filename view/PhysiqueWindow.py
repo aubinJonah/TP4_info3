2c9236aef724c417c677887378893c77685da675
aubinJonah <2346330@cegeplimoilou.ca>
2025-12-09 13:17:04 -0500

    changement moteur physique

--- view/PhysiqueWindow.py
+++ view/PhysiqueWindow.py
@@ -4,6 +4,7 @@ import pymunk
 from PyQt6.QtCore import QTimer, Qt
 from PyQt6.QtGui import QPixmap, QBrush, QPainter
 from PyQt6.QtWidgets import QWidget
+from pymunk import Vec2d
 
 
 class PhysiqueQtWidget(QWidget):
@@ -36,7 +37,7 @@ class PhysiqueQtWidget(QWidget):
         mass = 1
         self.size = (100,50)
         self.body = pymunk.Body(mass, pymunk.moment_for_box(mass, self.size))
-        self.body.position = (200, 100)
+        self.body.position = (0, 100)
         self.angle = -math.degrees(self.body.angle)
         shape = pymunk.Poly.create_box(self.body)
         shape.elasticity = 0.8
@@ -47,7 +48,9 @@ class PhysiqueQtWidget(QWidget):
         dt = 1 / 60
 
         self.space.step(dt)
+        print(f"{self.body.position.x},{self.body.position.y}")
         self.update()
+
 
     def paintEvent(self, event):
         p = QPainter(self)
@@ -57,28 +60,35 @@ class PhysiqueQtWidget(QWidget):
         p.drawRect(0,self.H - 50,self.W,50)
         # --- Dessine la voiture ---
         x = int(self.body.position.x)
-        y = int(self.body.position.y)
+        y = int(self.H -self.body.position.y)
+        self.angle = -math.degrees(self.body.angle)
         p.setBrush(Qt.GlobalColor.red)
+        p.translate(self.body.position.x,self.body.position.y)
         p.rotate(self.angle)
-        p.drawRect(x,y,self.size[0],self.size[1])
+        p.drawRect(x,self.H - y,self.size[0],self.size[1])
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key.Key_Up:
-            self.moveCar()
+            self.moveCar("forward")
+        if event.key() == Qt.Key.Key_Down:
+            self.moveCar("backward")
         if event.key() == Qt.Key.Key_Left:
             self.rotate_car("left")
         if event.key() == Qt.Key.Key_Right:
             self.rotate_car("right")
 
 
-    def moveCar(self):
-        self.body.apply_force_at_local_point((500,0),(0,0))
+    def moveCar(self,sens):
+        y = 50 * math.sin(self.body.angle)
+        x = 50 * math.cos(self.body.angle)
+
+        if sens == "forward":
+            self.body.velocity = Vec2d(x,y)
+        #else:
+            #elf.body.velocity = Vec2d(x,y)
 
     def rotate_car(self,sens):
         if sens == "left":
-            #self.body.apply_force_at_local_point((500,500),(self.size[0]/2,self.size[1]/2))
-            self.body.torque = 100
-            self.angle = -math.degrees(self.body.angle)
+            self.body.torque = 200
         if sens == "right":
-            #self.body.apply_force_at_local_point((500,500),(0,0))
-            pass
+            self.body.torque = -200
